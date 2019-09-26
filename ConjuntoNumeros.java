3d0e61284c3558dcea7801c89f18d28b6d3b2ae7
Walter López Araya <wallyjosuelopeza@estudiantestec.cr>
2019-09-26 14:58:45 -0600

    Se crea la clase de conjunto de números

---
+++ ConjuntoNumeros.java
@@ -0,0 +1,24 @@
+package Caso7_Desencriptar;
+
+import java.util.ArrayList;
+
+public class ConjuntoNumeros {
+	ArrayList<Integer> numeros = new ArrayList<Integer>();
+	double probabilidadConjunto = 0d;
+
+	public void agregarNumeros(ArrayList<Integer> pNumeros, int posicionInicio, int posicionFinal) {
+		while (posicionInicio < posicionFinal) {
+			numeros.add(pNumeros.get(posicionInicio));
+			posicionInicio++;
+		}
+	}
+
+	public double getProbabilidadConjunto() {
+		return probabilidadConjunto;
+	}
+
+	public void setProbabilidadConjunto(double probabilidadConjunto) {
+		this.probabilidadConjunto = probabilidadConjunto;
+	}
+
+}
